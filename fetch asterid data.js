31195a301de24f056f3a9c1bb787001f97a53db0
Toka <toka.ragheb.2010@gmail.com>
2025-02-14 20:14:35 +0200

    updated code

--- fetch asterid data.js
+++ fetch asterid data.js
@@ -1,23 +1,11 @@
-function analyzeAsteroidData(asteroidData) {
-    let asteroidDiameters = [];
+const apiKey = "iMtR0csGltCxENghPlFxUD3NX918oongG2YHS2PD";
+const startDate = "2025-02-01";
+const endDate = "2025-02-07";
+const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
 
-    for (const date in asteroidData) {
-        const asteroids = asteroidData[date];
-
-        asteroids.forEach(asteroid => {
-            const maxDiameter = asteroid.estimated_diameter.kilometers.estimated_diameter_max;
-            asteroidDiameters.push(maxDiameter);
-        });
-    }
-
-    const totalAsteroids = asteroidDiameters.length;
-    let averageDiameter = 0;
-
-    if (totalAsteroids > 0) {
-        const sumDiameters = asteroidDiameters.reduce((sum, diameter) => sum + diameter, 0);
-        averageDiameter = sumDiameters / totalAsteroids;
-    }
-
-    console.log("Total asteroids found:", totalAsteroids);
-    console.log("Average asteroid diameter:", averageDiameter.toFixed(3));
-}
+fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
